6d0db2213f791d4e204b57f79485a73af15049ef
H. Abbasi <72363836+abbasi08@users.noreply.github.com>
2022-02-04 14:46:49 -0500

    updated, Stanley recs

--- app.js
+++ app.js
@@ -6,29 +6,30 @@ const app = express();
 app.use(morgan('dev')); 
 app.use(express.static('public')); 
 
+const header = `<head>
+    <title>Polyamory</title>
+    <link rel="stylesheet" href="/style.css" />
+  </head>`
 
 app.get('/', (req, res) => {
     const books = bookData.list();
     const html = `<!DOCTYPE html>
-  <html>
-  <head>
-    <title>9 Books on Polyamory</title>
-    <link rel="stylesheet" href="/style.css" />
-  </head>
-  <body>
-    <div class="book-list">
-      <header><img src="/poly_logo2.jpg"/>9 Best Polyamory Books on Consensual Non-Monogamy</header>
-      ${books.map(book => `
-        <div class='book-item'>
-          <p>
-            <span class="book-position">${book.id}. ▲ </span>${book.title}
-            <small>by ${book.author}</small>
-          </p>
-        </div>`
-      ).join('')}
-    </div>
-  </body>
-</html>`
+        <html>
+            ${header}
+            <body>
+                <div class="book-list">
+                <header><img src="/poly_logo2.jpg"/> 9 Best Polyamory Books on Consensual Non-Monogamy</header>
+                ${books.map(book => `
+                    <div class='book-item'>
+                    <p>
+                        <span class="book-position">${book.id}. ▲ </span>${book.title}
+                        <small>by ${book.author}</small>
+                    </p>
+                    </div>`
+                ).join('')}
+                </div>
+            </body>
+        </html>`
 
 res.send(html)
 }); 
